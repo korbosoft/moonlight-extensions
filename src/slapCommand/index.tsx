45ec292a922f244f45016bab55c0438b950acfc9
Korbo <korbin.m.marshall@gmail.com>
2025-11-17 18:17:02 -0600

    make the plugin actually work lmao

--- src/slapCommand/index.tsx
+++ src/slapCommand/index.tsx
@@ -1,21 +1,11 @@
-import Commands from "@moonlight-mod/wp/commands_commands";
-import { InputType, CommandType } from "@moonlight-mod/types/coreExtensions/commands";
-import UserStore from "@moonlight-mod/wp/common_stores";
+import { ExtensionWebExports } from "@moonlight-mod/types";
 
-Commands.registerCommand({
-  type: CommandType.CHAT,
-  inputType: InputType.BUILT_IN,
-  id: "slap",
-  description: "Slaps the victim.",
-  options: [
-    {
-      name: "victim",
-      description: "The victim of the slapping",
-      type: OptionType.STRING
-    }
-  ],
-  execute: (options) => {
-    const victim = options.find((o): o is StringCommandOption => o.name === "victim")
-    return { content: `<@${UserStore.getCurrentUser().id}> slaps ${victim} around a bit with a large trout` };
+export const webpackModules: ExtensionWebExports["webpackModules"] = {
+  entrypoint: {
+    dependencies: [
+      { ext: "commands", id: "commands" },
+      { ext: "common", id: "stores" }
+    ],
+    entrypoint: true
   }
-});
+};
---
+++ src/slapCommand/webpackModules/entrypoint.ts
@@ -0,0 +1,22 @@
+import Commands from "@moonlight-mod/wp/commands_commands";
+import { CommandType, InputType, OptionType, StringCommandOption } from "@moonlight-mod/types/coreExtensions/commands";
+import { UserStore } from "@moonlight-mod/wp/common_stores";
+import { User } from "@moonlight-mod/types";
+
+Commands.registerCommand({
+    type: CommandType.CHAT,
+    inputType: InputType.BUILT_IN_TEXT,
+    id: "slap",
+    description: "Slaps the victim.",
+    options: [
+        {
+            name: "victim",
+            description: "The victim of the slapping",
+            type: OptionType.STRING
+        }
+    ],
+    execute: (options) => {
+        const victim = options.find((o): o is StringCommandOption => o.name === "victim")
+        return { content: `<@${UserStore.getCurrentUser().id}> slaps ${victim.value} around a bit with a large trout` };
+    }
+});
